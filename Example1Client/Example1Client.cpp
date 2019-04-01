8c9f3003481c092c812684fde2d26e3eb81c78a1
Alexandr Kolesov <sharutenshow@gmail.com>
2019-04-01 23:43:52 +0300

    Implemented user operations.

--- Example1Client/Example1Client.cpp
+++ Example1Client/Example1Client.cpp
@@ -1,67 +1,215 @@
-// File Example1Client.cpp
 #include <iostream>
+#include <string>
+
 #include "../Example1/Example1.h"
+#include "../Example1/ReturnCodes.h"
+
+using namespace std;
+
+bool logged_in = false;
+string login;
+string password;
+
+void help()
+{
+	cout << "help [h] - show this help" << endl;
+	cout << "register - register a new account" << endl;
+	cout << "login - login using existing account" << endl;
+	cout << "logout - logout from the current account" << endl;
+	cout << "list - list all users" << endl;
+}
+
+void client_sign_on()
+{
+	string login, password;
+	cout << "Login: ";
+	cin >> login;
+	cout << "Password: ";
+	cin >> password;
+
+	user_dto u;
+	strcpy(u.login, login.c_str());
+	strcpy(u.password, password.c_str());
+
+	int result = sign_up(u);
+
+	switch (result)
+	{
+	case RC_OK:
+		cout << "Ok" << endl;
+		break;
+	case RC_USER_ALREADY_EXISTS:
+		cout << "The login is already taken" << endl;
+		break;
+	default: 
+		break;
+	}
+}
+
+void client_sign_in()
+{
+	string l, p;
+	cout << "Login: ";
+	cin >> l;
+	cout << "Password: ";
+	cin >> p;
+
+	user_dto u;
+	strcpy(u.login, l.c_str());
+	strcpy(u.password, p.c_str());
+
+	int result = sign_in(u);
+
+	switch (result)
+	{
+	case RC_OK:
+		login = l;
+		password = p;
+		logged_in = true;
+		cout << "Ok" << endl;
+		break;
+	case RC_USER_DOES_NOT_EXIST:
+		cout << "Invalid credentials" << endl;
+		break;
+	default:
+		break;
+	}
+}
+
+void sign_out()
+{
+	login = "";
+	password = "";
+	logged_in = false;
+
+	cout << "Ok" << endl;
+}
+
+void warn_login()
+{
+	cout << "Please login first" << endl;
+}
+
+void list_users()
+{
+	user_dto buffer[2];
+	int offset = 0;
+	int read;
+
+	while (read = get_users(buffer, offset, 2), read != 0)
+	{
+		for (int i = 0; i < read; i++)
+		{
+			cout << buffer[i].login << endl;
+		}
+
+		offset += read;
+	}
+}
+
+void loop()
+{
+	while (true)
+	{
+		string command;
+		cin >> command;
+
+		if (command == "help" || command == "h")
+		{
+			help();
+			continue;
+		}
+
+		if (command == "register")
+		{
+			client_sign_on();
+			continue;
+		}
+
+		if (command == "login")
+		{
+			client_sign_in();
+			continue;
+		}
+
+		if (command == "logout")
+		{
+			sign_out();
+			continue;
+		}
+
+		if (command == "list")
+		{
+			if (!logged_in)
+			{
+				warn_login();
+				continue;
+			}
+
+			list_users();
+			continue;
+		}
+
+		cout << "Unknown command. Please use help." << endl;
+	}
+}
 
 int main()
 {
-   RPC_STATUS status;
-   unsigned char* szStringBinding = NULL;
+	RPC_STATUS status;
+	unsigned char* szStringBinding = NULL;
 
-   // Creates a string binding handle.
-   // This function is nothing more than a printf.
-   // Connection is not done here.
-   status = RpcStringBindingCompose(
-      NULL, // UUID to bind to.
-      reinterpret_cast<unsigned char*>("ncacn_ip_tcp"), // Use TCP/IP
-                                                        // protocol.
-      reinterpret_cast<unsigned char*>("localhost"), // TCP/IP network
-                                                     // address to use.
-      reinterpret_cast<unsigned char*>("4747"), // TCP/IP port to use.
-      NULL, // Protocol dependent network options to use.
-      &szStringBinding); // String binding output.
+	// Creates a string binding handle.
+	// This function is nothing more than a printf.
+	// Connection is not done here.
+	status = RpcStringBindingCompose(
+		NULL, // UUID to bind to.
+		reinterpret_cast<unsigned char*>("ncacn_ip_tcp"), // Use TCP/IP
+		// protocol.
+		reinterpret_cast<unsigned char*>("localhost"), // TCP/IP network
+		// address to use.
+		reinterpret_cast<unsigned char*>("4747"), // TCP/IP port to use.
+		NULL, // Protocol dependent network options to use.
+		&szStringBinding); // String binding output.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 
-   // Validates the format of the string binding handle and converts
-   // it to a binding handle.
-   // Connection is not done here either.
-   status = RpcBindingFromStringBinding(
-      szStringBinding, // The string binding to validate.
-      &hExample1Binding); // Put the result in the implicit binding
-                          // handle defined in the IDL file.
+	// Validates the format of the string binding handle and converts
+	// it to a binding handle.
+	// Connection is not done here either.
+	status = RpcBindingFromStringBinding(
+		szStringBinding, // The string binding to validate.
+		&hExample1Binding); // Put the result in the implicit binding
+	// handle defined in the IDL file.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 
-   RpcTryExcept
-   {
-      // Calls the RPC function. The hExample1Binding binding handle
-      // is used implicitly.
-      // Connection is done here.
-      Output("Hello Implicit RPC World!");
-   }
-   RpcExcept(1)
-   {
-      std::cerr << "Runtime reported exception " << RpcExceptionCode()
-                << std::endl;
-   }
-   RpcEndExcept
+	RpcTryExcept
+		{
+			loop();
+		}
+	RpcExcept(1)
+		{
+			std::cerr << "Runtime reported exception " << RpcExceptionCode() << std::endl;
+		}
+	RpcEndExcept
 
-   // Free the memory allocated by a string.
-   status = RpcStringFree(
-      &szStringBinding); // String to be freed.
+	// Free the memory allocated by a string.
+	status = RpcStringFree(
+		&szStringBinding); // String to be freed.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 
-   // Releases binding handle resources and disconnects from the server.
-   status = RpcBindingFree(
-      &hExample1Binding); // Frees the implicit binding handle defined in
-                          // the IDL file.
+	// Releases binding handle resources and disconnects from the server.
+	status = RpcBindingFree(
+		&hExample1Binding); // Frees the implicit binding handle defined in
+	// the IDL file.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 }
 
 // Memory allocation function for RPC.
@@ -69,11 +217,11 @@ int main()
 // enough memory to pass the string to the server.
 void* __RPC_USER midl_user_allocate(size_t size)
 {
-    return malloc(size);
+	return malloc(size);
 }
 
 // Memory deallocation function for RPC.
 void __RPC_USER midl_user_free(void* p)
 {
-    free(p);
+	free(p);
 }
--- Example1Server/Example1Server.cpp
+++ Example1Server/Example1Server.cpp
@@ -1,57 +1,140 @@
-// File Example1Server.cpp
 #include <iostream>
-#include "../Example1/Example1.h"
+#include <string>
+#include <vector>
+#include <mutex>
 
-// Server function.
-void Output( 
-   /* [string][in] */ const char* szOutput)
+#include "../Example1/Example1.h"
+#include "../Example1/ReturnCodes.h"
+
+using namespace std;
+
+class user
 {
-   std::cout << szOutput << std::endl;
+public:
+	string login;
+	string password;
+	int last_activity;
+};
+
+class message
+{
+public:
+	user* from;
+	user* to;
+	string body;
+	int timestamp;
+	bool read;
+};
+
+vector<user*> users;
+
+user* get_user(string login)
+{
+	for (auto u : users)
+	{
+		if (u->login == login)
+		{
+			return u;
+		}
+	}
+
+	return nullptr;
 }
+
+int sign_up(user_dto dto)
+{
+	auto u = get_user(dto.login);
+
+	if (u != nullptr)
+	{
+		cout << "User already exists: " + u->login << endl;
+		return RC_USER_ALREADY_EXISTS;
+	}
+
+	u = new user();
+	u->login = dto.login;
+	u->password = dto.password;
+
+	users.push_back(u);
+
+	cout << "User signed up: " + u->login << endl;
+	return RC_OK;
+}
+
+int sign_in(user_dto dto)
+{
+	auto u = get_user(dto.login);
+
+	if (u != nullptr)
+	{
+		cout << "User signed in: " + u->login << endl;
+		return RC_OK;
+	}
+
+	cout << "User sign in error: " << dto.login << endl;
+	return RC_USER_DOES_NOT_EXIST;
+}
+
+int get_users(user_dto* buffer, int offset, int count)
+{
+	int read = 0;
+
+	for (auto i = offset; i < offset + count && i < users.size(); i++)
+	{
+		strcpy(buffer[read].login, users[i]->login.c_str());
+		strcpy(buffer[read].password, "");
+
+		read++;
+	}
+
+	return read;
+}
+
+/* STARTUP */
 
 // Naive security callback.
 RPC_STATUS CALLBACK SecurityCallback(RPC_IF_HANDLE /*hInterface*/, void* /*pBindingHandle*/)
 {
-    return RPC_S_OK; // Always allow anyone.
+	return RPC_S_OK; // Always allow anyone.
 }
 
 int main()
 {
-   RPC_STATUS status;
+	RPC_STATUS status;
 
-   // Uses the protocol combined with the endpoint for receiving
-   // remote procedure calls.
-   status = RpcServerUseProtseqEp(
-      reinterpret_cast<unsigned char*>("ncacn_ip_tcp"), // Use TCP/IP protocol.
-      RPC_C_PROTSEQ_MAX_REQS_DEFAULT, // Backlog queue length for TCP/IP.
-      reinterpret_cast<unsigned char*>("4747"), // TCP/IP port to use.
-      NULL); // No security.
+	// Uses the protocol combined with the endpoint for receiving
+	// remote procedure calls.
+	status = RpcServerUseProtseqEp(
+		reinterpret_cast<unsigned char*>("ncacn_ip_tcp"), // Use TCP/IP protocol.
+		RPC_C_PROTSEQ_MAX_REQS_DEFAULT, // Backlog queue length for TCP/IP.
+		reinterpret_cast<unsigned char*>("4747"), // TCP/IP port to use.
+		NULL); // No security.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 
-   // Registers the Example1 interface.
-   status = RpcServerRegisterIf2(
-      Example1_v1_0_s_ifspec, // Interface to register.
-      NULL, // Use the MIDL generated entry-point vector.
-      NULL, // Use the MIDL generated entry-point vector.
-      RPC_IF_ALLOW_CALLBACKS_WITH_NO_AUTH, // Forces use of security callback.
-      RPC_C_LISTEN_MAX_CALLS_DEFAULT, // Use default number of concurrent calls.
-      (unsigned)-1, // Infinite max size of incoming data blocks.
-      SecurityCallback); // Naive security callback.
+	// Registers the Example1 interface.
+	status = RpcServerRegisterIf2(
+		Example1_v1_0_s_ifspec, // Interface to register.
+		NULL, // Use the MIDL generated entry-point vector.
+		NULL, // Use the MIDL generated entry-point vector.
+		RPC_IF_ALLOW_CALLBACKS_WITH_NO_AUTH, // Forces use of security callback.
+		1, //RPC_C_LISTEN_MAX_CALLS_DEFAULT, // Use default number of concurrent calls.
+		(unsigned)-1, // Infinite max size of incoming data blocks.
+		SecurityCallback); // Naive security callback.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 
-   // Start to listen for remote procedure calls for all registered interfaces.
-   // This call will not return until RpcMgmtStopServerListening is called.
-   status = RpcServerListen(
-      1, // Recommended minimum number of threads.
-      RPC_C_LISTEN_MAX_CALLS_DEFAULT, // Recommended maximum number of threads.
-      FALSE); // Start listening now.
+	// Start to listen for remote procedure calls for all registered interfaces.
+	// This call will not return until RpcMgmtStopServerListening is called.
+	status = RpcServerListen(
+		1, // Recommended minimum number of threads.
+		RPC_C_LISTEN_MAX_CALLS_DEFAULT, // Recommended maximum number of threads.
+		FALSE); // Start listening now.
 
-   if (status)
-      exit(status);
+	if (status)
+		exit(status);
 }
 
 // Memory allocation function for RPC.
@@ -59,11 +142,11 @@ int main()
 // enough memory to pass the string to the server.
 void* __RPC_USER midl_user_allocate(size_t size)
 {
-    return malloc(size);
+	return malloc(size);
 }
 
 // Memory deallocation function for RPC.
 void __RPC_USER midl_user_free(void* p)
 {
-    free(p);
+	free(p);
 }
